70f2c4127f4c1656d0a04aa6831aec8aa05a97c2
MiniCubo <minicubo121@gmail.com>
2026-03-03 11:41:26 -0600

    Done all of the Fragments and Hardware, left to do the calculator and imc calculator

---
+++ app/src/main/java/com/example/frragmentosasvr/Inicio.kt
@@ -0,0 +1,77 @@
+package com.example.frragmentosasvr
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Inicio.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Inicio : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_inicio, container, false)
+        val mensaje2 = view.findViewById<Button>(R.id.mensaje2)
+        mensaje2.setOnClickListener {
+            (activity as MainActivity).mensaje()
+        }
+        val qr = view.findViewById<Button>(R.id.qr)
+        qr.setOnClickListener {
+            (activity as MainActivity).leerQR()
+        }
+        val gps = view.findViewById<Button>(R.id.gps)
+        val ubicacion = view.findViewById<TextView>(R.id.gps_text)
+        gps.setOnClickListener {
+            (activity as MainActivity).obtenerUbicacion { ubicacionObtenida ->
+                ubicacion.text = ubicacionObtenida.toString()
+            }
+        }
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Inicio.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Inicio().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/example/frragmentosasvr/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.frragmentosasvr
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
+class MainActivity : AppCompatActivity() {
+    private val barcodeLauncher = registerForActivityResult<ScanOptions?, ScanIntentResult?>(
+        ScanContract(), { result: ScanIntentResult? ->
+            if(result!!.contents == null) Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+            else Toast.makeText(this, "Scanned ${result.contents}", Toast.LENGTH_SHORT).show()
+        }
+    )
+
+    private lateinit var fusedLocationClient : FusedLocationProviderClient
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()){
+        permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) ->{}
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {}
+                else -> {}
+            }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val calculadora = findViewById<Button>(R.id.Calculadora)
+        val inicio = findViewById<Button>(R.id.Inicio)
+        val imc = findViewById<Button>(R.id.imc)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        calculadora.setOnClickListener { taskId ->
+            replaceFragment(Calculadora())
+        }
+        inicio.setOnClickListener { taskId ->
+            replaceFragment(Inicio())
+        }
+        imc.setOnClickListener { taskId ->
+            replaceFragment(Imc())
+        }
+    }
+
+    fun replaceFragment(fragmento : Fragment){
+        val manager = supportFragmentManager
+        val transaccion = manager.beginTransaction()
+        transaccion.replace(R.id.fragmentContainerView, fragmento).addToBackStack(null)
+        transaccion.commit()
+    }
+
+    fun mensaje(){
+        Toast.makeText(this, "mensaje desde la actividad main", Toast.LENGTH_SHORT).show()
+    }
+
+    fun leerQR(){
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan a QR CODE")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        barcodeLauncher!!.launch(options)
+    }
+
+    fun requestPermissions(){
+        locationPermissionRequest.launch(
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        )
+    }
+
+    fun obtenerUbicacion(callback : (String) -> Unit){
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions()
+            return
+        }
+        else{
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                val ubicacion = location?.toString() ?: "Ubicacion no disponible"
+                callback(ubicacion)
+            }
+        }
+    }
+}
